9131f40508f99a01ea1f8868628abaa03f3d6270
Nhat Pham <hoangnhat123456@live.com>
2018-09-28 09:50:09 -0700

    LC 478

---
+++ LeetCode/478. Generate Random Point in a Circle/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    double radius;
+    double x_center;
+    double y_center;
+
+    public Solution(double radius, double x_center, double y_center) {
+        this.radius = radius;
+        this.x_center = x_center;
+        this.y_center = y_center;
+    }
+
+    public double[] randPoint() {
+        boolean found = false;
+        double[] ret = new double[2];
+        Random randomizer = new Random();
+
+        while (!found) {
+            double x = randomizer.nextDouble() * 2 * radius + x_center - radius;
+            double y = randomizer.nextDouble() * 2 * radius + y_center - radius;
+
+            if ((x_center - x) * (x_center - x) + (y_center - y) * (y_center - y) <= radius * radius) {
+                ret[0] = x;
+                ret[1] = y;
+                found = true;
+            }
+        }
+
+        return ret;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(radius, x_center, y_center);
+ * double[] param_1 = obj.randPoint();
+ */
