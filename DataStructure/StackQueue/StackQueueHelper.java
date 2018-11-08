22a84dd0bfe24a68000335bdafcd2f155e485bd1
Nhat Pham <hoangnhat123456@live.com>
2018-11-07 17:55:49 -0800

    Add reverse method for stack and queue.

--- DataStructure/StackQueue/StackQueueHelper.java
+++ DataStructure/StackQueue/StackQueueHelper.java
@@ -2,14 +2,26 @@ import java.util.*;
 
 public class StackQueueHelper {
 
-  public void q2s(Queue<Integer> q, Stack<Integer> s) {
+  public <E> void q2s(Queue<E> q, Stack<E> s) {
     while (!q.isEmpty())
       s.push(q.remove());
   }
 
-  public void s2q(Stack<Integer> s, Queue<Integer> q) {
+  public <E> void s2q(Stack<E> s, Queue<E> q) {
     while (!s.isEmpty())
       q.add(s.pop());
+  }
+
+  public <E> void reverseStack(Stack<E> s) {
+    Queue<E> q = new LinkedList<E>();
+    s2q(s, q);
+    q2s(q, s);
+  }
+
+  public <E> void reverseQueue(Queue<E> q) {
+    Stack<E> s = new Stack<E>();
+    q2s(q, s);
+    s2q(s, q);
   }
 
 }
