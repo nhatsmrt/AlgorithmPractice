5f5bb6fdaab62537ec747c9f4af972f9a047225b
nhatsmrt <nhatsmrt@uw.edu>
2019-07-11 08:19:29 -0700

    LC 100-101

---
+++ LeetCode/100. Same Tree/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null)
+            return q == null;
+        if (q == null)
+            return p == null;
+
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
---
+++ LeetCode/101. Symmetric Tree/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+
+        return isSymmetricHelper(root.left, root.right);
+
+    }
+
+    private boolean isSymmetricHelper(TreeNode p, TreeNode q) {
+        if (p == null)
+            return q == null;
+        if (q == null)
+            return p == null;
+
+        if (p.val != q.val)
+            return false;
+
+        return isSymmetricHelper(p.left, q.right) && isSymmetricHelper(p.right, q.left);
+    }
+
+}
