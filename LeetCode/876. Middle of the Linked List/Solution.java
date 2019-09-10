2bf01d9fbca76cefee205aa7ebe61f4372b9f923
nhatsmrt <nhatsmrt@uw.edu>
2019-09-09 18:29:55 -0700

    LC 876

---
+++ LeetCode/876. Middle of the Linked List/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+}
