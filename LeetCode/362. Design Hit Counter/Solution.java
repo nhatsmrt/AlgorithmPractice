dbc29057a6ea4c86e533475d17d779dee1529dc6
nhatsmrt <nhatsmrt@uw.edu>
2019-12-17 05:45:48 -0800

    LC 362

---
+++ LeetCode/362. Design Hit Counter/Solution.java
@@ -0,0 +1,29 @@
+class HitCounter {
+    private Queue<Integer> hits;
+
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        hits = new LinkedList<Integer>();
+    }
+
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        hits.add(timestamp);
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while(!hits.isEmpty() && hits.peek() + 300 <= timestamp)
+            hits.remove();
+        return hits.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Topics included:
   * Multi-Level: LC 430 (Flatten)
 4. Stack, Queue, Deque: LC 225 (Implement Stack using Queues), LC 232 (Implement Queue using Stacks)
   * Stack: LC 20 (Valid Parentheses), LC 106, LC 150 (Reverse Polish Notation), LC 155 (Min Stack), LC 173, LC 224 + 227 (Infix to Postfix/Dijkstra's Shunting yard algorithm), LC 234, LC 402, LC 581, LC 636, LC 716 (Max Stack), LC 735, LC 889, LC 946
-  * Queue: LC 105, LC 106, LC 158, LC 346, LC 621 (Task Scheduler), LC 622 (Circular Queue), LC 889
+  * Queue: LC 105, LC 106, LC 158, LC 346, LC 362 (Design Hit Counter), LC 621 (Task Scheduler), LC 622 (Circular Queue), LC 889
   * Deque: LC 84, LC 199, LC 341, LC 456, LC 641 (Circular Deque), LC 862, LC 901, LC 907
   * Monotonicity: LC 85, LC 402, LC 456, LC 581, LC 862, LC 901, LC 907
   * Parentheses:  LC 20, LC 394
