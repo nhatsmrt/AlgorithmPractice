e9451741cad4ef0653366b828ba9384ce2568823
nhatsmrt <nhatsmrt@uw.edu>
2020-01-11 12:47:24 -0500

    Non-destructively reverse linked list.

---
+++ HackerRank/Reverse a linked list/Solution.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+        if (head == null)
+            return null;
+
+        SinglyLinkedListNode ret = reverse(head, null);
+        return reverse(head, null);
+    }
+
+    static SinglyLinkedListNode reverse(SinglyLinkedListNode node, SinglyLinkedListNode copyPrev) {
+        SinglyLinkedListNode copy = new SinglyLinkedListNode(node.data);
+        copy.next = copyPrev;
+        return node.next == null ? copy : reverse(node.next, copy);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
