ba174c2f512ce44a39528f3efbfa0ffc98cf763b
nhatsmrt <nhatsmrt@uw.edu>
2020-01-12 14:31:13 -0500

    Add a cleaner solution to LC 203.

---
+++ LeetCode/203. Remove Linked List Elements/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution2 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return null;
+
+        if (head.val == val)
+            return removeElements(head.next, val);
+
+        head.next = removeElements(head.next, val);
+        return head;
+    }
+
+}
