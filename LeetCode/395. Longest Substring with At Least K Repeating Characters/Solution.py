2af5a2797f0b30f3cdc36195bb3189ecf71853dd
nhatsmrt <nhatsmrt@uw.edu>
2020-09-05 16:19:00 -0700

    Update time complexity.

--- LeetCode/395. Longest Substring with At Least K Repeating Characters/Solution.py
+++ LeetCode/395. Longest Substring with At Least K Repeating Characters/Solution.py
@@ -6,7 +6,7 @@ def get_char(ind) -> str:
 
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        # Time and Space Complexity: O(N^2) (worst case)
+        # Time and Space Complexity: O(N) (worst case, since there is at most 26 levels, and total number of work per level is O(N))
 
         cum_freq = [[0 for _ in range(26)] for i in range(len(s) + 1)]
         for i in range(1, len(s) + 1):
