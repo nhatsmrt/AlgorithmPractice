3173bbeefac58eb19ab61d89c441b293538c8078
nhatsmrt <nhatsmrt@uw.edu>
2020-10-22 08:24:24 -0700

    LC 111

---
+++ LeetCode/111. Minimum Depth of Binary Tree/Solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        elif not any((root.left, root.right)):
+            return 1
+        elif not root.left:
+            return 1 + self.minDepth(root.right)
+        elif not root.right:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
